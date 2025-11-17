73d7f6e7b62d424f3b75409a958016f4fd68baac
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-11-16 21:14:49 -0800

    add service and env

---
+++ src/app/weather/weather-service/weather-service.service.ts
@@ -0,0 +1,20 @@
+// weather.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class WeatherService {
+  private readonly baseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+  
+
+  getRandomWeather() {
+    return this.http.get(`${this.baseUrl}/weather/random`);
+  }
+
+  getWeatherByCity(city: string) {
+    return this.http.get(`${this.baseUrl}/weather?city=${encodeURIComponent(city)}`);
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:50000', // your local .NET API
+};
