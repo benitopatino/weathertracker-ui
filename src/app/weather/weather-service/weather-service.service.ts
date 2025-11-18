6ce66f5694239bd73f43c866f2f939c68f1c970b
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-11-17 20:02:40 -0800

    complete weather service

--- src/app/weather/weather-service/weather-service.service.ts
+++ src/app/weather/weather-service/weather-service.service.ts
@@ -2,6 +2,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { CityWeather } from '../models/city-weather';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
@@ -9,8 +11,8 @@ export class WeatherService {
 
   constructor(private http: HttpClient) {}
   
-  getWeatherByCity(city: string) {
+  getWeatherByCity(city: string): Observable<CityWeather>  {
     const url = `${this.baseUrl}/temperature/weather/${encodeURIComponent(city)}`;
-    return this.http.get(url);
+    return this.http.get<CityWeather>(url);
   }
 }
